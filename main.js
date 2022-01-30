d358f95867118bb6099b67081ca2e7540a104bda
Honorable con <honorablecon@gmail.com>
2022-01-30 11:21:58 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import {backup, initializeClient} from "./supabackup.js";
+import {backup, initializeClient} from "supabackup";
+import {config} from 'dotenv';
+config();
 
-const SUPABASE_URL="database url"
-const SERVICE_KEY='service_role secret'
+const SUPABASE_URL=process.env.SUPABASE_URL
+const SERVICE_KEY=process.env.SUPABASE_SERVICE_KEY
 
-const tables = ['table1','table2','table3','table4']
+const tables = ['table1','table2','table3']
 const bucket = "Backup"
 const supabackup = initializeClient(SUPABASE_URL, SERVICE_KEY);
 
